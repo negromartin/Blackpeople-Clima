5799826f5d63c6cf1cc21a445aea8c80bc1c9019
negromartin <martin.retamozo12@gmail.com>
2020-02-08 20:36:48 -0300

    Updates

---
+++ precache-manifest.4b2ea6b28b5adaba94f032ce4f3aec09.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9ca5433704f27f2ca2850e0f5f16232",
+    "url": "/negromartin/Blackpeople-Clima.git/index.html"
+  },
+  {
+    "revision": "2802fa21c79f600b7400",
+    "url": "/negromartin/Blackpeople-Clima.git/static/css/main.a23b3a59.chunk.css"
+  },
+  {
+    "revision": "0537a2b5b4c97e511584",
+    "url": "/negromartin/Blackpeople-Clima.git/static/js/2.1b059ef0.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/negromartin/Blackpeople-Clima.git/static/js/2.1b059ef0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2802fa21c79f600b7400",
+    "url": "/negromartin/Blackpeople-Clima.git/static/js/main.9b83ea8e.chunk.js"
+  },
+  {
+    "revision": "b708ef737fb3f9f44cee",
+    "url": "/negromartin/Blackpeople-Clima.git/static/js/runtime-main.6036e008.js"
+  }
+]);
